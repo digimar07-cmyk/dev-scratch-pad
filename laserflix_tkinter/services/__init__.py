dfc0c7c8b960dc669c063824ea426424e98d3310
digimar07-cmyk <digimar07@gmail.com>
2026-02-27 00:45:41 -0300

    feat: update services/__init__.py with all service exports
    Add ImageService and AnalysisService to exports

--- laserflix_tkinter/services/__init__.py
+++ laserflix_tkinter/services/__init__.py
@@ -1,4 +1,6 @@
 """Services module for Laserflix."""
 from .ollama_service import OllamaService
+from .image_service import ImageService
+from .analysis_service import AnalysisService
 
-__all__ = ["OllamaService"]
+__all__ = ["OllamaService", "ImageService", "AnalysisService"]
